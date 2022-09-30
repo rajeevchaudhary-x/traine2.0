39c05ff36dd960e904678586e155b5c37be4dc9e
Rajeev choudhary <91446278+rajeevchoudhary035@users.noreply.github.com>
2022-09-30 12:48:13 +0530

    Drdo project files

---
+++ form.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        h1{
+          font-size: 15px;
+          margin-left:470px ;
+        }
+        h2{
+            font-size: 15px;
+            margin-left: 590px;
+        }
+       div.td1.td
+        {
+            font-display: 6px;
+            width: 1000px;
+            
+        }
+        form{
+            margin-top: -90px;
+margin-left: 280px;
+        }
+        sign{
+            margin-left: 800px;
+            font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+            font-size: small;
+        }
+        SO{
+            margin-left: 530px;
+            font-size: small;
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        }
+        ND{
+            margin-left: 600px;
+            font-size: small;
+            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; 
+        }
+    </style>
+</head>
+<body>
+    <!-----------------Heading-------------->
+    <div class="head">
+        <h1>INSTITUTE OF TECHNOLOGY MANAGEMENT, MUSSOORE</h1>
+       <h2>NOMINATION FORM</h2> 
+    </div>
+    <form>
+        
+        <table border="1" cellspacing="0">
+            <tr>
+               <div class="td1"> <td colspan="10">
+                    NAME OF TRAINING PROGRAMME   
+                                <br>
+                    DATE OF TRAINING PROGRAMME    
+                </td></div>
+                <td colspan="10">
+                    <!--<input type="text"placeholder="  " name="">-->
+                </td>
+                <tr>
+                    <td colspan="7">TITLE(pls Tick)<br>
+                    Prof./Dr./Mr./Mrs.</td>
+                    <td colspan="3">FULL NAME</td>
+                    <td colspan="10">
+
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="6">
+                        Name in Hindi
+                    </td>
+                    <td colspan="14">
+
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5">DESIGNATION</td>
+                    <td colspan="5"></td>
+                    <td colspan="5">NAME OF LAB/<BR>
+                    ESTABLISHMENT</td>
+                    <td colspan="5"></td>
+                </tr>
+                <tr>
+                    <td colspan="5">DATE OF BIRTH</td>
+                    <td colspan="5"></td>
+                    <td colspan="5">PIS NUMBER</td>
+                    <td colspan="5"></td>
+                </tr>
+                <tr>
+                    <td colspan="5">SEX (M/F)</td>
+                    <td colspan="15"></td>
+                </tr>
+                <tr>
+                    <td colspan="6">PRESENT NATURE OF<BR>
+                    JOB</td>
+                    <td colspan="14"></td>
+                </tr>
+                <TR>
+                    <td colspan="6">MOBILE NUMBERS</td>
+                    <td colspan="14"></td>
+                </TR>
+                <tr>
+                    <td colspan="5">E-MAIL(INTERNET)</td>
+                    <td colspan="5"></td>
+                    <td colspan="5">E-MAIL(DRONA)</td>
+                    <td colspan="5"></td>
+                </tr>
+                <tr>
+                    <td colspan="20"> <br></td>
+                </tr>
+                <tr>
+                    <td colspan="20">TRAINING ATTENDED(In last 03 years</td>
+                </tr>
+                <tr>
+                    <td colspan="4">SL.NO</td>
+                    <td colspan="4">YEAR</td>
+                    <td colspan="4">NAME OF THE TRAINING PROGRAMME</td>
+                    <td colspan="4">NAME OF THE INSTITUTE</td>
+                    <td colspan="4">DURATION</td>
+                </tr>
+                <tr height="70px";>
+                    <td colspan="4"></td>
+                    <br>
+                    <td colspan="4"></td>
+<br>
+                    <td colspan="4"></td>
+<br>
+                    <td colspan="4"></td>
+<br>
+                    <td colspan="4"></td>
+                    <br>
+                </tr>
+                <tr>
+                    <td colspan="20">ASSIGNMENT HELD IN LAST 03 YEARS</td>
+                </tr>
+                <tr>
+                    <td colspan="4">SL.NO</td>
+                    <td colspan="8">NAME OF PROJECT</td>
+                    <td class="8"> ROLE OF INDIVIDUAL</td>
+                </tr>
+                <tr height="70px";>
+                    <td colspan="2"></td>
+                    <td colspan="9"></td>
+                    <td colspan="9"></td>
+                </tr>
+                <tr>
+                    <td colspan="20">ETA</td>
+                </tr>
+                <tr>
+                    <td colspan="20">
+                        Pick Up AT
+                    </td>
+                </tr>
+               <div class="check1"> <tr>
+                    <td colspan="20" height="250px";>
+                       <c1> Arrival<br>
+                        Date:
+                    
+                        <input type="checkbox" name="Arrival">DDN STN
+                        <input type="checkbox" name="Arrival">Jolly grant
+                        <input type="checkbox" name="Arrival">ISBT DDN</c1>
+                   
+                <br>
+                <br>
+                            <c2>
+                                Time Slot
+                                <input type="checkbox" name="Time Slot">
+                                <input type="checkbox" name="Time Slot">
+                                <input type="checkbox" name="Time Slot">
+                            </c2>
+                            <br>
+                            <br>
+                            <br>
+                            <c3>
+                                Departure<br>
+                                 Date:
+                                <input type="checkbox" name="Time Slot">DDN STN
+                                <input type="checkbox" name="Time Slot">Jolly Grant
+                                <input type="checkbox" name="Time Slot">ISBT DDN
+                            </c3>
+                            <br>
+                            <br>
+                            <c4>
+                                Time Slot
+                                <input type="checkbox" name="Time Slot">
+                                <input type="checkbox" name="Time Slot">
+                                <input type="checkbox" name="Time Slot">
+                            </c4>
+                            <br>
+                            <br>
+                            <p>If info not pick up from DDN cannot be arranged Tpt shall be provided on sharing basis only.</p>
+                        </td>
+                    </div>
+        </table>
+    </form>
+    <br>
+    <br>
+    <br>
+    <sign>SIGNATURE OF THE NOMINEE</sign>
+    <BR>
+        <BR>
+            <br>
+    <SO>(SIGNATURE OF THE RECOMMENDING OFFICER)</SO>
+    <BR>
+        <ND>NAME & DESIGNATION WITH SEAL</ND>
+</body>
+</html>
---
+++ latter.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nomination Letter</title>
+    <style>
+        *{
+            margin: 0px;
+            padding: 0px;
+        }
+        h1{
+font-size: 18px;
+margin-left: 300px;
+        }
+        f{
+            margin-left: 340px;
+        }
+        date{
+            margin-left: 600px;
+        }
+        subject{
+            margin-left: 170px;
+        }
+        ref{
+            margin-left: 110px;
+        }
+        td{
+            width: 240px;
+        }
+        dot1{
+            margin-left: 110px;
+        }
+        dot2{
+            margin-left: 10px;
+        }
+        dot3{
+            margin-left:165px ;
+        }
+    </style>
+</head>
+<body>
+    <h1><i><u>Nomination Letter</i></u></h1>
+    <f>Letter No. of Lab/Estt: ... ... ... ... ... ... ... ... ... ... ... ... ... ...</f>
+    <br>
+    <date>Date. ... ... ... ... ...</date>
+    <br>
+    <br>
+    <to>To</to>
+    <br>
+    <p>The Director<br>Centre for Personnel Talent Management(CEPTAM)<BR>
+    Metcalfe House, Civil Lines<br>
+Delhi - 110 054</p>
+<br>
+<subject>Subject: Nomination for Training Course</subject>
+<br>
+<ref>Ref: DRDO/CEPTAM/TD/30508/2022-2023 dated 22/08/2022</ref>
+<br>
+<br>
+<course>Course Title:   ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...</course>
+<br>
+<br>
+<schedule>Schedule:    ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...  Venue:   ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... .</schedule>
+<br>
+<br>
+<table border="1px" cellspacing="0" >
+    <tr>
+        <td colspan="10"></td>
+        <td colspan="10">Nomination No. ... ... ...    </td>
+        <td colspan="10">Nomination No. ... ... ...    </td>
+    </tr>
+    <tr>
+        <td colspan="10">Name(Block Letter)</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Gender</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Rank</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Qualification(with discipline</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Date of Birth</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Personal Information No. (PIN)/PIS</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Mobile</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">E-mail Address</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Date of Joining in Present Rank</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Present Area of Work Do not mention<br>
+        group/division name</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Last Training Attended(Title & Year)</td>
+        <td colspan="10"></td>
+        <td colspan="10"></td>
+    </tr>
+    <tr>
+        <td colspan="10">Is candidate PWD?<sup>03</sup></td>
+        <td colspan="10">YES/NO</td>
+        <td colspan="10">YES/NO</td>
+    </tr>
+
+</table>
+<br>
+<br>
+<br>
+<p>Name of the Lab/Estt:<dot1>   ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...</dot1></p>
+<br>
+<p>Name & Designation of Head, HRD: <dot2>   ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...</dot2></p>
+<br>
+<p>Mobile No.: <dot3></dot3>... ... ... ... ... ... ... ... ...  .   Signature: ... ... ... ... ... ... ... ... ...</p>
+<br>
+<br>
+<note>Note: ------------------------------------------------------------------------------------------------------------------------------------------</note>
+<br>
+<t1>#1.<u>Nominations which are not received in this form will be rejected without assigning any reason. </u></t1>
+<br>
+<t2>#2. It mandatory to fill in all fields of this form for being consider by CEPTAM.</t2>
+<br>
+<t3>#3. In case of PWD candidate, labs are requested to communicate separately to CEPTAM Training Division.</t3>
+<br>
+
+<dot4>--------------------------------------------------------------------------------------------------------------------------------------------------</dot4>
+<br>
+<fax>Fax:022 - 23817489 011 - 23810287                                   Phone: 011 - 23882347, 011 - 23882300</fax>
+
+    
+</body>
+</html>
